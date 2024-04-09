cd1e2f926182aef291d6d9d30e04ff9a383d6bb4
Dimash <mwasikzano@gmail.com>
2024-04-09 10:32:56 +0500

    Spring MVC. Конфигурация с помощью Java кода.

---
+++ src/main/java/kz/dim/springcourse/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package kz.dim.springcourse.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+// zamena web xml
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
