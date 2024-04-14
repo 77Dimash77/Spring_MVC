18fe72db2da07e8b9dc451fcd1a8db91e1d11c2b
Dimash <mwasikzano@gmail.com>
2024-04-14 12:59:10 +0500

    Model

--- src/main/java/kz/dim/springcourse/controllers/FirstController.java
+++ src/main/java/kz/dim/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package kz.dim.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,14 +13,43 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name", required = false) String name, @RequestParam (value = "surname", required = false) String surname){
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam (value = "surname", required = false) String surname, Model model){
 
-        System.out.println(name + " " + surname);
+       // System.out.println(name + " " + surname);
+        model.addAttribute("message","Hello" + name + " " + surname);
 
 return "first/hello";
     }
     @GetMapping("/goodbye")
     public String goodByePage(){
 return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a, @RequestParam("b") int b,
+                             @RequestParam("action") String action, Model model){
+double result;
+switch (action){
+    case  "mult":
+        result = a * b;
+        break;
+    case  "del":
+        result = a /(double) b;
+        break;
+    case  "plus":
+        result = a + b;
+        break;
+    case  "minus":
+        result = a - b;
+        break;
+
+    default: result = 0;
+
+}
+model.addAttribute("result", result);
+
+
+        return "first/calculator";
     }
 }
